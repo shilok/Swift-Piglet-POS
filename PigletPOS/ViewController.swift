1b8c9f83f0d296df789c78c174df06bc7d1b9158
shilok <s.kohelet@gmail.com>
2019-06-13 03:37:27 +0300

    Initial Commit

---
+++ PigletPOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PigletPOS
+//
+//  Created by Shilo Kohelet on 13/06/2019.
+//  Copyright © 2019 Shilo Kohelet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
